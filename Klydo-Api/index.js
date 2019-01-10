47bbdc9ad6c659af49db77df1b49c86c5266660a
Nivid Vaidya <nivid12@gmail.com>
2019-01-10 12:20:09 +0530

    Build No : 10012019.1 Message : Test commit.

--- Klydo-Api/index.js
+++ Klydo-Api/index.js
@@ -8,7 +8,7 @@ const express = require('express');
 var app = express();
 var authenticate = loadSecurity('authenticate');
 
-app.use(express.urlencoded({ extended: true })); // support encoded bodies
+app.use(express.urlencoded({extended: true})); // support encoded bodies
 app.use(express.json());
 
 TODO:'use concat in user names where neccessary'
@@ -18,28 +18,28 @@ app.post('/app/v0/login/signup', loadController('LoginController').signupUser);
 app.post('/app/v0/login/forget', loadController('LoginController').forgetPassword);
 app.post('/app/v0/login/forgot-password-code', loadController('LoginController').forgetPasswordVerificationCode);
 //validate user before calling any routes
-app.use(async (req, res, next) => {	
-	if(req.originalUrl === '/app/v0/login/authenticate' || req.originalUrl === '/app/v0/login/signup' 
-	|| req.originalUrl === '/app/v0/login/forget' || req.originalUrl === '/app/v0/login/forget-password-code') {
-		next();			
-	}else {
-		// let [verification,err] = await catchError(authenticate.validateToken(req.headers.token));
-		// if(err) {
-		// 	console.log(err);
-		// 	res.json({auth: false, msg:'Oops! Something unexpected happened. Please try again.'});
-		// } else {
-		// 	if(verification.auth) {
-		// 		//All Application routes
-				loadRoute('route')(app,express);
-				next();
-			// } else {
-			// 	res.json(verification);
-			// }
-		// }
+app.use(async (req, res, next) => {
+    if (req.originalUrl === '/app/v0/login/authenticate' || req.originalUrl === '/app/v0/login/signup'
+        || req.originalUrl === '/app/v0/login/forget' || req.originalUrl === '/app/v0/login/forget-password-code') {
+        next();
+    } else {
+        // let [verification,err] = await catchError(authenticate.validateToken(req.headers.token));
+        // if(err) {
+        // 	console.log(err);
+        // 	res.json({auth: false, msg:'Oops! Something unexpected happened. Please try again.'});
+        // } else {
+        // 	if(verification.auth) {
+        // 		//All Application routes
+        loadRoute('route')(app, express);
+        next();
+        // } else {
+        // 	res.json(verification);
+        // }
+        // }
 
-	}
+    }
 });
 
 app.listen(3000, () => {
-	console.log('Listening on 3000');
+    console.log('Listening on 3000');
 });
