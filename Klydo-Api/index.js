fc02373fdb163f24cda98fb0ae029fdc9342e8c7
Yashesh Tagariwala <tagariwalayashesh@gmail.com>
2019-12-04 19:02:15 +0530

    Email Verification Api Added

--- Klydo-Api/index.js
+++ Klydo-Api/index.js
@@ -21,7 +21,9 @@ let urls = [
     '/sandbox/v0/login/forget-password-code',
     '/sandbox/v0/graph/trendsLogin',
     '/sandbox/v0/login/loginMedia',
-    '/sandbox/v1/send-push'
+    '/sandbox/v1/send-push',
+    '/sandbox/v2/login/verify-email',
+    '/sandbox/v2/login/verify-email/:code',
 ];
 
 app.post('/sandbox/v0/login/authenticate', loadController('LoginController').loginCheck);
@@ -31,6 +33,8 @@ app.get('/sandbox/v0/login/trendsLogin', loadController('LoginController').getTr
 app.get('/sandbox/v0/login/loginMedia/:filename', loadController('LoginController').LoginMedia);
 app.post('/sandbox/v0/login/forgot-password-code', loadController('LoginController').forgetPasswordVerificationCode);
 app.post('/sandbox/v1/send-push',loadV1Controller('PushNotification').sendMessagePush);
+app.post('/sandbox/v2/verify-email',loadController('LoginController').verifyEmail);
+app.get('/sandbox/v2/verify-email/:code',loadController('LoginController').verifyEmailCode);
 
 //validate user before calling any routes
 app.use(async (req, res, next) => {
